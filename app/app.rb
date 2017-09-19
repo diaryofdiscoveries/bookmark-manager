542286a89235fe993ec54f85db658ae75323a593
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-19 15:28:41 +0100

    Configuring the Racing Environment

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,10 @@
+ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 # We're gonna need our Link model
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+
   get '/links' do
     # This uses DataMapper's .all method to fetch all
     # data pertaining to this class from the database
