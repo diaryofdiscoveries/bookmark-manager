2bd9e69414bc5135cc9450e2943994f58ed9b8bc
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-20 19:58:11 +0100

    Change data mapper setup from auto_update to auto_migrate

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,7 @@ class Link
   # add DataMapper functionality to this class so it can communicate with the database
   include DataMapper::Resource
 
-   has n, :tags#, through: Resource
+   has n, :tags, through: Resource
 
   # these property declarations set the column headers in the 'links' table
   property :id,     Serial  # Serial means that it will be auto-incremented for every record
