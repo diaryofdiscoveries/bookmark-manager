c1a6b9baef21cc2ad9094e96e27f87f124459516
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-22 11:17:43 +0100

    Password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,24 @@ class User
 
   include DataMapper::Resource
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  # validates_confirmation_of is a DataMapper method
+  # provided especially for validating confirmation passwords!
+  # The model will not save unless both password
+  # and password_confirmation are the same
+  # read more about it in the documentation
+  # http://datamapper.org/docs/validations.html
+  validates_confirmation_of :password
+
   property :id, Serial
   property :email, String
   # this will store both the password and the salt
- # It's Text and not String because String holds
- # only 50 characters by default
- # and it's not enough for the hash and salt
- property :password_digest, Text
+  # It's Text and not String because String holds
+  # only 50 characters by default
+  # and it's not enough for the hash and salt
+  property :password_digest, Text
 
  # when assigned the password, we don't store it directly
   # instead, we generate a password digest, that looks like this:
@@ -20,6 +31,7 @@ class User
   # has both the password hash and the salt. We save it to the
   # database instead of the plain password for security reasons.
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,4 +6,11 @@ feature 'User sign up' do
     expect(page).to have_content('Welcome, zoe@example.com')
     expect(User.first.email).to eq('zoe@example.com')
   end
+
+  scenario 'requires a matching confirmation password' do
+    # again it's questionable whether we should be testing the model at this
+    # level.  We are mixing integration tests with feature tests.
+    # However, it's convenient for our purposes.
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
 end
